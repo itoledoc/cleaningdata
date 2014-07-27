780f7bb79f3f3d3d959be548c2df8f65fa9476af
Ignacio Toledo <itoledoc@gmail.com>
2014-07-27 17:45:16 -0400

    final version of the analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -25,22 +25,33 @@ y_test[y_test['V1'] == 6] = "laying"
 
 names(subject_test) = "subject"
 names(subject_train) = "subject"
-names(X_train) = features$V2
-names(X_test) = features$V2
+
+newnames <- sub('\\()','c', features$V2)
+newnames <- sub('-','.', newnames)
+
+names(X_train) = newnames
+names(X_test) = newnames
 names(y_test) = "activity"
 names(y_train) = "activity"
-int_cols <-append(grep('.*mean.*', names(X_test)), grep('.*std.*', names(X_test)))
+int_cols <-append(grep('.*meanc', names(X_test)), grep('.*stdc', names(X_test)))
 
 X_train_clean <- X_train[int_cols]
 X_test_clean <- X_test[int_cols]
+
+names(X_train_clean) <- sub('meanc','mean',names(X_train_clean))
+names(X_train_clean) <- sub('stdc','std',names(X_train_clean))
+
+names(X_test_clean) <- sub('meanc','mean',names(X_test_clean))
+names(X_test_clean) <- sub('stdc','std',names(X_test_clean))
 
 test_df <- cbind(subject_test, y_test, X_test_clean)
 train_df <- cbind(subject_train, y_train, X_train_clean)
 
 tidy1_df <- rbind.data.frame(test_df, train_df)
+names(tidy1_df) <- sub('-','.',names(tidy1_df))
 
 write.table(tidy1_df, file="clean_set.csv")
-test <- read.table("clean_set.csv")
-df_an <- summaryBy(list(names(test)[3:81], c("subject","activity")), data=test, FUN=c(mean,var))
+
+df_an <- summaryBy(list(names(tidy1_df)[3:81], c("subject","activity")), data=tidy1_df, FUN=c(mean,sd))
 
 write.table(df_an, file="summary_set.csv")
